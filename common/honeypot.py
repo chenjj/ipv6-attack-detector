69dfa2507d23f03773197aaaaf145f1f5d7dc387
whucjj <whucjj@gmail.com>
2013-07-25 17:02:20 +0800

    mv extension-header-checker from honeypot.py to exthdr.py

--- common/honeypot.py
+++ common/honeypot.py
@@ -10,6 +10,7 @@ from scapy.all import *
 import logger
 from common import *
 import message
+from exthdr import *
 
 # The class Honeypot emultates an IPv6 host.
 # TODO: Generate MAC address with specified vendor (or prefix).
@@ -212,33 +213,7 @@ class Honeypot(threading.Thread):
     def do_invalid_exthdr(self, pkt):
         if self.check_host_discovery_bynmap(pkt) == 1:
             return 1
-        self.check_extheader_order(pkt)
-        return 0
-
-    #Check the order and count of extension header options
-    #ret: 0: Valid extension header, do nothing
-    #ret: 1: The order or count of extension header is invalid, log the event, 
-    #When more than one extension header is used in the same packet, it is recommended that those headers appear in the following order[RFC 2460, 1998]:
-    #IPv6 header, Hop-by-Hop Options header, Destination Options header, Routing header, Fragment header, Authentication header, Encapsulating Security Payload header, Destination Options header, Upper-layer header
-    #Each extension header should occur at most once, except for the Destination Options header which should occur at most twice (once before a Routing header and once before the upper-layer header)[RFC 2460]
-    def check_extheader_order(self,pkt):
-        #the below values are defined in RFC2460.
-        next_headers_vals = [0, 60, 43, 44, 51, 50, 60, 135, 59, 6, 17, 58]
-        pkt_index = 0
-        header_val_index = 0
-        while "IPv6ExtHdr" in pkt[pkt_index].summary() and header_val_index < 8:
-            if pkt[pkt_index].nh == next_headers_vals[header_val_index]:
-                pkt_index=pkt_index + 1
-                header_val_index = header_val_index + 1
-            else:
-                header_val_index = header_val_index + 1
-        if header_val_index >=8 and "IPv6ExtHdr" in pkt[pkt_index].summary():
-            msg = self.msg.new_msg(pkt, save_pcap = 0)
-            msg['type'] = "Invalid Extension Header"
-            msg['name'] = "Invalid Extension Header in packets"
-            msg['util'] = "Crafting malformed Packets"
-            self.msg.put_event(msg)
-            return 1
+        check_extheader_order(pkt)
         return 0
 
     # Handle the IPv6 invalid extention header options. (One of Nmap's host discovery technique.)
