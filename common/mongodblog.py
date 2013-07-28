6b55fcd52190010a6c8935f61355a942e1d9160c
whucjj <whucjj@gmail.com>
2013-07-28 14:31:09 +0800

    add mongodb log module to 6guard

---
+++ common/mongodblog.py
@@ -0,0 +1,25 @@
+import dblog
+import time
+import pymongo
+
+class MongoDBLogger(dblog.DBLogger):
+    def get_db(host, port, name, user = '', passwd = ''):
+        dbconn = pymongo.Connection(host, port)
+        db = pymongo.database.Database(dbconn, name)
+        db.authenticate(user, passwd)
+        return db
+
+    def start(self, cfg):
+        self.host = config.get("database_mongodb","host")
+        self.port = config.get("database_mongodb","port")
+        self.user = config.get("database_mongodb","user")
+        self.password = config.get("database_mongodb","password")
+        self.database = config.get("database_mongodb","database")
+        self.collection = config.get("database_mongodb","collection"
+        self.db = get_db(self.host, self.port, self.database,self.user,self.password)
+
+    def write(self, msg):
+        self.db[self.collection].insert(msg)
+
+    def close(self):
+        self.db.close()
